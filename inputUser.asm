e8a2c9991cedb200b2d6d4f29e4ad2587d981491
Decta <mo.a.baghyani@gmail.com>
2025-12-09 02:32:41 +0330

    0.0.1

--- inputUser.asm
+++ inputUser.asm
@@ -8,5 +8,25 @@ section .bss
 section .text 
     global _start
 
-_start 
+_start:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, msg_len
+  syscall
 
+  mov rax, 0
+  mov rdi, 0
+  mov rsi, buffer
+  mov rdx, 64
+  syscall
+
+  mov rdi, 1
+  mov rsi, buffer
+  mov rdx, rax
+  mov rax, 1
+  syscall
+
+  mov rax, 60
+  mov rdi, 0
+  syscall
